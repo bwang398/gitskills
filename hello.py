5ea47d19589ea6c81abf2c3aa4313530cd13d40c
wangbing02326 <wangbing02326@hellobike.com>
2019-11-19 23:57:47 +0800

    解决冲突

--- hello.py
+++ hello.py
@@ -3,8 +3,5 @@ git@github.com:bwang398/gitskills.git
 Creating a new branch is quick AND simple.
 ++++++++++++++++++++++++++
 bug修复
-<<<<<<< HEAD
-=======
 ------------------------------------------
 新增加一个分支
->>>>>>> dev
